e9015974d0e26f0d6ccb748945961170f77de3fd
Denis Dovgan <den.dovgan@gmail.com>
2016-05-09 18:17:05 +0300

    Initial Commit

---
+++ MediaController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MediaController
+//
+//  Created by Denis Dovgan on 5/9/16.
+//  Copyright © 2016 Denis Dovgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
